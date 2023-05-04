c50ed9a0303b3067917b20fb25f8700fb3c5149b
biru3 <49255122+biru3@users.noreply.github.com>
2023-05-05 01:16:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,94 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-const dotenv = require('dotenv'); 
-dotenv.config();
+const { Client , Intents , Collection}  = require('discord.js')
+const client = new Client({intents:32767})
+const fs = require('fs')
+const mongoose = require("mongoose")
 
-const sleep = (ms) => {
-    return new Promise((r) => setTimeout(r, ms));
-}
+mongoose.connect("mongodb+srv://chio2:ss388500@cluster0.qfjwj0c.mongodb.net/?retryWrites=true&w=majority", {
+useNewUrlParser: true ,  useUnifiedTopology: true 
+}).then(console.log("데이터베이스 연결 완료"))
+const { prefix, token} =require('./config.json')
+const { DiscordTogether } = require('discord-together')
+client.discordTogether = new DiscordTogether(client);
 
-if (process.env.TOKEN == null) {
-    console.log("An discord token is empty.");
-    sleep(60000).then(() => console.log("Service is getting stopped automatically"));
-    return 0;
-}
+client.once('ready',()=>{
+  let number = 0
+  setInterval(() => {
+      const list = ["치오야 도움말", "음악 요청","문의"]
+      if(number == list.length) number = 0
+      client.user.setActivity(list[number],{
+          type:"LISTENING"
+      })
+      number++
+  }, 2000) //몇초마다 상태메세지를 바꿀지 정해주세요 (1000 = 1초)
+  console.log("치오 START")
+})
 
-const discordLogin = async() => {
-    try {
-        await client.login(process.env.TOKEN);  
-    } catch (TOKEN_INVALID) {
-        console.log("An invalid token was provided");
-        sleep(60000).then(() => console.log("Service is getting stopped automatically"));
+client.on('messageCreate' , message=>{
+    if(message.content == "치오야"){
+        message.reply("네? 무슨 일이에요.")
     }
+})
+client.commands = new Collection()
+
+const commandsFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for(const file of commandsFile){
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name , command)
 }
 
-
-discordLogin();
-
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}.`);
-});
-
-  
-client.on('messageCreate', msg => {
-
-    try { 
-        if (msg.content === process.env.PREFIX + 'call') msg.channel.send(`!callback`);
-
-        if (msg.content === process.env.PREFIX + 'avatar') msg.channel.send(msg.author.displayAvatarURL());
-        
-        if(msg.content === process.env.PREFIX + 'help') {
-            const embed = new Discord.MessageEmbed()
-            .setTitle("도움말")
-            .setColor('000') 
-            .setDescription('디스코드봇 테스트입니다.');
-
-            msg.reply({ embeds: [embed] })
-        }
-
-        if(msg.content === process.env.PREFIX + 'server') {
-            msg.channel.send(`현재 서버의 이름은 ${msg.guild.name} 입니다.\n총 멤버 수는 ${msg.guild.memberCount} 명 입니다.`)
-          }
-
-        console.log(msg.content)
-    } catch (e) {
-        console.log(e);
+client.on('messageCreate' , message=>{
+    if(!message.content.startsWith(prefix)) return
+    const args = message.content.slice(prefix.length).trim().split(/ +/)
+    const commandName = args.shift()
+    const command = client.commands.get(commandName)
+    if (!command) return
+    try{
+        command.execute(message,args)
+    } catch (error) {
+        console.error(error)
     }
-    
-});
+})
+
+client.on('messageCreate',message=>{
+  if(message.content == `${prefix}유튜브`){
+      const channel = message.member.voice.channel
+      if(!channel) return message.reply("먼저 음성채널에 접속해주세요!")
+      client.discordTogether.createTogetherCode(channel.id, 'youtube').then(invite =>{
+          return message.channel.send(invite.code)
+      })
+  }
+})
+
+client.on('messageCreate',message=>{
+  if(message.content == `${prefix}포커`){
+      const channel = message.member.voice.channel
+      if(!channel) return message.reply("먼저 음성채널에 접속해주세요!")
+      client.discordTogether.createTogetherCode(channel.id, 'poker').then(invite =>{
+          return message.channel.send(invite.code)
+      })
+  }
+})
+
+client.on('messageCreate',message=>{
+  if(message.content == `${prefix}체스`){
+      const channel = message.member.voice.channel
+      if(!channel) return message.reply("먼저 음성채널에 접속해주세요!")
+      client.discordTogether.createTogetherCode(channel.id, 'chess').then(invite =>{
+          return message.channel.send(invite.code)
+      })
+  }
+})
+
+client.on('messageCreate',message=>{
+  if(message.content == `${prefix}스케치헤드`){
+      const channel = message.member.voice.channel
+      if(!channel) return message.reply("먼저 음성채널에 접속해주세요!")
+      client.discordTogether.createTogetherCode(channel.id, 'sketchheads').then(invite =>{
+          return message.channel.send(invite.code)
+      })
+  }
+})
+
+
+client.login()
